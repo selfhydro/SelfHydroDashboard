455e4d1556511adb130961cceef1e7f1861be35f
Baz <boruch11@gmail.com>
2019-05-24 10:42:11 +1000

    First commit

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	client "github.com/influxdata/influxdb/client/v2"
+	mqtt "github.com/selfhydro/selfhydro/mqtt"
+	"github.com/selfhydro/selfhydro/sensors"
+)
+
+const (
+	SefhydroStateDB = "selfhydro"
+)
+
+type SeflhydroState struct {
+	WaterTemperature float64 `json:"waterTemperature"`
+	Time             string  `json:"time"`
+	deviceId         string
+}
+
+func main() {
+	// Create a new HTTPClient
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr: "http://localhost:8086",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	// Create a new point batch
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  SefhydroStateDB,
+		Precision: "s",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	localMQTT := mqtt.NewLocalMQTT("influxdb-ingester")
+	localMQTT.ConnectDevice()
+
+	waterTemp := &sensors.WaterTemperature{}
+	waterTemp.Subscribe(localMQTT)
+	ambientTemperature := &sensors.AmbientTemperature{}
+	ambientTemperature.Subscribe(localMQTT)
+	ambientHumidity := &sensors.AmbientHumidity{}
+	ambientHumidity.Subscribe(localMQTT)
+	electricalConductivity := &sensors.WaterEC{}
+	electricalConductivity.Subscribe(localMQTT)
+	time.Sleep(time.Second * 10)
+
+	for {
+		waterTemperature := waterTemp.GetLatestData()
+		ambientTemp := ambientTemperature.GetLatestData()
+		currentAmbientHumidity := ambientHumidity.GetLatestData()
+		currentElectricalConductivity := electricalConductivity.GetLatestData()
+		fmt.Println("water temp: ", waterTemperature)
+
+		tags := map[string]string{"device": "selfhydro"}
+		temperatureFields := map[string]interface{}{
+			"waterTemperature":   waterTemperature,
+			"ambientTemperature": ambientTemp,
+		}
+		humidityFields := map[string]interface{}{
+			"ambientHumidity": currentAmbientHumidity,
+		}
+		electricalConductivityFields := map[string]interface{}{
+			"electricalConductivity": currentElectricalConductivity,
+		}
+
+		tempPt, err := client.NewPoint("temperature", tags, temperatureFields, time.Now())
+		humidityPt, err := client.NewPoint("humidity", tags, humidityFields, time.Now())
+		electricalConductivityPt, err := client.NewPoint("electricalConductivity", tags, electricalConductivityFields, time.Now())
+		if err != nil {
+			log.Fatal(err)
+		}
+		bp.AddPoint(tempPt)
+		bp.AddPoint(humidityPt)
+		bp.AddPoint(electricalConductivityPt)
+
+		if err := c.Write(bp); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := c.Close(); err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(time.Minute * 5)
+	}
+}
